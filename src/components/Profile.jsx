ad7597bf385c1903a5bb53a1ed09156b8fa7fb52
crashberry <crashberry@gmail.com>
2024-06-01 19:39:31 +0300

    add Profile grid

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,3 +1,26 @@
 export const Profile = () => {
-  return <h1>Профиль</h1>;
+  return (
+    <div className="row">
+      <div className="col-md-4">
+        <img
+          src="https://img.freepik.com/free-psd/3d-illustration-bald-person-with-glasses_23-2149436184.jpg"
+          alt="Аватар"
+        />
+      </div>
+      <p>
+        <span>Id:</span>
+        <span>2</span>
+      </p>
+      <p>
+        <span>E-mail:</span>
+        <span>vania@mail.ru</span>
+      </p>
+      <p>
+        <span>Фамилия и имя:</span>
+        <span>Иван Иванов</span>
+      </p>
+      <p>Рассказ о себе</p>
+      <div className="col-md-8"></div>
+    </div>
+  );
 };
