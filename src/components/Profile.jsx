47c12ea0866bfa63c701865c6811420ba5a7e079
crashberry <crashberry@gmail.com>
2024-06-02 14:14:54 +0300

    change getUser

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,7 +6,8 @@ export const Profile = (props) => {
     fontSize: "1.2rem",
   };
 
-  let user = props.function();
+  let userId = window.location.pathname.split("/")[2];
+  let user = props.function(userId);
 
   return (
     <div className="row">
