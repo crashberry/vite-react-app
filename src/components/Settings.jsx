924ca0f911f520f236ed8a406f6f68950d35afa1
crashberry <crashberry@gmail.com>
2024-06-02 18:02:53 +0300

    add class component to Settings

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 const TableRow = (props) => {
@@ -14,6 +15,37 @@ const TableRow = (props) => {
     </tr>
   );
 };
+
+export class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { userRow: [] };
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="row">
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Имя и фамилия</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">Id</th>
+              </tr>
+            </thead>
+            <tbody>{this.state.userRow}</tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
 
 export const Settings = (props) => {
   let users = props.function();
@@ -33,22 +65,4 @@ export const Settings = (props) => {
       />
     );
   }
-
-  return (
-    <div className="row">
-      <div className="row">
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Имя и фамилия</th>
-              <th scope="col">E-mail</th>
-              <th scope="col">Id</th>
-            </tr>
-          </thead>
-          <tbody>{userRow}</tbody>
-        </table>
-      </div>
-    </div>
-  );
 };
